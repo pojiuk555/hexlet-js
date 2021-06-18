8dfdd5409189397c979c8155c9ad3d9caf4e6ee0
pojiuk555 <pojiuk555@gmail.com>
2021-06-18 12:58:47 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import _ from "lodash";
 
-console.log('Hello, Hexlet!');
-
-console.log(_.last(['one', 'two']));
+const fn = (a, b) => {                                                                                                                                                                     
+    const c = a + b;                                                                                                                                                                         
+    return c;                                                                                                                                                                                
+  };                                                                                                                                                                                         
+     
