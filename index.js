fae9aed07750bb5eca0fa337776ad73cc4172dc7
pojiuk555 <pojiuk555@gmail.com>
2021-06-29 12:00:45 +0300

    add and modiefy

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const fn = (a, b) => {
-  const c = a + b;
-  return c;
+import _ from 'lodash';
+
+const functions = {
+  right1: _.get,
+  fail1: (obj = {}, key = null) => obj[key],
+  fail2: (obj = {}, key = null, defaultValue = null) => defaultValue || obj[key],
+  fail3: (obj = {}, key = null, defaultValue = null) => ((obj[key] && !defaultValue)
+    ? null
+    : _.get(obj, key, defaultValue)
+  ),
 };
-fn(1, 2);
+
+export default () => {
+  const name = process.env.FUNCTION_VERSION || 'right1';
+  return functions[name];
+};
---
+++ test/test.index.js
@@ -0,0 +1,16 @@
+import getFunction from '../index.js';
+
+const get = getFunction();
+
+// BEGIN (write your solution here)
+if (get({ hello: 'world' }, 'hello') !== 'world') {
+  throw new Error('Функция работает неверно!');
+}
+
+if (get({ hello: 'world' }, '', 'kitty') !== 'kitty') {
+  throw new Error('Функция работает неверно!');
+}
+
+if (get({ hello: 'world' }, 'hello', 'kitty') !== 'world') {
+  throw new Error('Функция работает неверно!');
+}
